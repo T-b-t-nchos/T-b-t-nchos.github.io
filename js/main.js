93691569503992eaf66bdc0357ee30ca701f0ff9
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-08-09 00:18:41 +0900

    chore: move the script to the directory and subdivide it into multiple parts

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const img = document.getElementById("swapImg");
+
+const images = [
+    "./assets/single-color-icon.png",
+    "./assets/musasabi.png"
+];
+
+let index = 0;
+let isAnimating = false;
+
+img.addEventListener("click", () => {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    img.classList.add("rotate");
+
+    setTimeout(() => {
+        index = (index + 1) % images.length;
+        img.src = images[index];
+
+        img.classList.remove("rotate");
+        isAnimating = false;
+    }, 600);
+});
+
+
+const birthYear = 2011;
+const birthMonth = 12;
+
+const today = new Date();
+
+let age = today.getFullYear() - birthYear;
+
+if (today.getMonth() + 1 < birthMonth) {
+    age--;
+}
+
+document.getElementById("age").textContent = age;
+
