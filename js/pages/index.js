744768604a47521a0e1e639e59144640c6602c30
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-08-13 20:46:22 +0900

    refactor: use .hv-ul-intro as animation trigger

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll(".hv-ul");
+    const elements = document.querySelectorAll(".hv-ul-intro");
 
     elements.forEach((el) => {
         el.classList.add("intro");
